2d3727020a61f318472cbcdfdefcd08dec0d8f0c
Pinkel Gurung <pgurung@admarketplace.com>
2020-08-27 10:54:19 -0400

    structure grid

---
+++ src/components/ColumnBucket/TrafficSources/trafficSources.ts
@@ -0,0 +1,11 @@
+interface iData {
+  id: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  city: string;
+  country: string;
+  phone: string;
+  email: string;
+  company: string;
+}
