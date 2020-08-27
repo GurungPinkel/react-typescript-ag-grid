85bbaa066a3008c629947732d0bda0b07ba0f8a1
Tom Herrmann <therrmann@admarketplace.com>
2020-08-27 14:19:51 -0400

    Pass modal button methods as props

--- src/components/Grid/EditModal.tsx
+++ src/components/Grid/EditModal.tsx
@@ -1,13 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { AgGridReact } from "ag-grid-react";
-import { GridReadyEvent } from "ag-grid-community";
-import ExampleCellComponent from "../ExampleCellComponent";
+import React, { useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import { useDispatch } from 'react-redux';
+import { modalOpenSet } from '../../actions/actions';
+import {
+  GridReadyEvent,
+  NewValueParams,
+  RowNode,
+  Column,
+} from 'ag-grid-community';
 
-import "ag-grid-enterprise/dist/styles/ag-grid.css";
-import "ag-grid-enterprise/dist/styles/ag-theme-material.css";
+import ExampleCellComponent from '../ExampleCellComponent';
+import TransitionsModal from '../TransitionsModal';
 
-import { columnDef } from "./columnDef";
-import { exampleRowData } from "./exampleRowData";
+import 'ag-grid-enterprise/dist/styles/ag-grid.css';
+import 'ag-grid-enterprise/dist/styles/ag-theme-material.css';
+
+import { columnDef } from './columnDef';
+import { exampleRowData } from './exampleRowData';
 
 const frameworkComponents = {
   exampleCellComponent: ExampleCellComponent,
@@ -15,27 +24,74 @@ const frameworkComponents = {
 
 const EditModal = () => {
   const [rowData] = useState(exampleRowData);
-  // const [];
+  const [newCellValue, setNewCellValue] = useState<string>();
+  const [oldCellValue, setOldCellValue] = useState<string>();
+  const [colId, setColId] = useState<string | Column>('');
+  const [rowNode, setRowNode] = useState<RowNode>();
+  const dispatch = useDispatch();
 
   const onGridReady = (params: GridReadyEvent): void => {
-    console.log("test");
+    console.log('test');
     console.log(params);
+  };
+
+  /**
+   * Performs a series of actions when a cell is edited
+   * @param params - event obj with with both the old and new values
+   * Actions:
+   *  1. Validate the new value is acceptable
+   *  2. Display Modal to ensure user wants to make changes
+   *  3. Async put new data to backend
+   *  4. Display loading spinner and snackbar to confirm changes
+   */
+  const onCellValueChanged = (params: NewValueParams) => {
+    const { colDef, newValue, node, oldValue } = params;
+
+    console.log('params', params);
+
+    // save param values to state for modal button click handlers
+    setNewCellValue(newValue);
+    setOldCellValue(oldValue);
+    setColId(colId);
+    setRowNode(node);
+
+    // temp validation
+    if (newValue.length > 1) {
+      dispatch(modalOpenSet(true));
+    }
+  };
+
+  const handleModalClose = (): void => {
+    dispatch(modalOpenSet(false));
+
+    if (rowNode) rowNode.setDataValue(colId, oldCellValue);
+  };
+
+  const handleModalSubmit = (): void => {
+    // put new data to the backend
+    // loading spinner
+    // snackbar
   };
 
   return (
     <div
       className="ag-theme-material"
-      style={{ height: "500px", width: "100%" }}
+      style={{ height: '500px', width: '100%' }}
     >
+      <TransitionsModal
+        handleModalClose={handleModalClose}
+        handleModalSubmit={handleModalSubmit}
+      />
       <AgGridReact
         columnDefs={columnDef}
         rowData={rowData}
+        onCellValueChanged={onCellValueChanged}
         defaultColDef={{
           sortable: true,
         }}
         rowSelection="multiple"
         frameworkComponents={frameworkComponents}
-        debug={process.env.NODE_ENV === "development"}
+        debug={process.env.NODE_ENV === 'development'}
         onGridReady={onGridReady}
       ></AgGridReact>
     </div>
--- src/components/TransitionsModal.tsx
+++ src/components/TransitionsModal.tsx
@@ -1,36 +1,39 @@
-import React from "react";
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../reducers";
-import { modalOpenSet } from "../actions";
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../reducers';
+import { modalOpenSet } from '../actions';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     modal: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
     },
     paper: {
       backgroundColor: theme.palette.background.paper,
-      border: "2px solid #000",
+      border: '2px solid #000',
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
     },
   })
 );
 
-const TransitionsModal = () => {
+interface TransitionsModalProps {
+  handleModalClose: () => void;
+  handleModalSubmit: () => void;
+}
+
+const TransitionsModal = ({
+  handleModalClose,
+  handleModalSubmit,
+}: TransitionsModalProps) => {
   const classes = useStyles();
   const { modalOpen } = useSelector((state: RootState) => state.reducer);
-  const dispatch = useDispatch();
-
-  const handleModalClose = (): void => {
-    dispatch(modalOpenSet(false));
-  };
 
   return (
     <div>
@@ -53,7 +56,7 @@ const TransitionsModal = () => {
             <p id="transition-modal-description">
               react-transition-group animates me.
             </p>
-            <button>Save Changes</button>
+            <button onClick={handleModalSubmit}>Save Changes</button>
           </div>
         </Fade>
       </Modal>
