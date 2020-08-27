e751414469cf2d4f61b4978797b786b0e29a09bf
Tom Herrmann <therrmann@admarketplace.com>
2020-08-27 11:40:33 -0400

    Create RootState interface

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,11 @@
-import { combineReducers } from 'redux';
-import { reducer } from './reducer';
+import { combineReducers } from "redux";
+import { reducer } from "./reducer";
+import { State } from "./reducerTypes";
+
+export interface RootState {
+  reducer: State;
+}
 
 export default combineReducers({
-  reducer
+  reducer,
 });
